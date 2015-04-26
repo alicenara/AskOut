2f68ddbc3d2f11acc19e095279ed7e912622892b
Alice <alicenara165@gmail.com>
2015-04-26 20:47:35 +0200

    omgwhatshappening2

--- server/server.js
+++ server/server.js
@@ -5,14 +5,14 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require ('body-parser');
 var path = require('path');
-var config = require('/config');
+//var config = require('/config');
 
 var app = express();
 
 /*
 * Connect to the DB
 */
-mongoose.connect(config.database);
+//mongoose.connect(config.database);
 
 
 // send our index.html file to the user for the home page <- sha de canviar!
@@ -42,6 +42,7 @@ app.use('/admin', adminRouter);
 app.use('/api', apiRoutes);*/
 
 // start the server
-app.listen(config.port);
+//app.listen(config.port);
+app.listen(80);
 //console.log('1337 leet lol is the magic port!');
 console.log('server on');
