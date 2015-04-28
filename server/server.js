e7194f8e53ced848560c83e9a7a83f2b21393218
Alice <alicenara165@gmail.com>
2015-04-28 13:26:06 +0200

    trying

--- server/server.js
+++ server/server.js
@@ -49,10 +49,10 @@ app.get('/getData',function(req,res){
 /************************************
 Routes examples, not required -> admin
 *************************************/
-//var adminRouter = require('./app/routes/admin')(app,express);
+var adminRouter = require('./app/routes/admin')(app,express);
 
 // apply the routes to our application
-//app.use('/admin', adminRouter);
+app.use('/admin', adminRouter);
 //app.use('/api', apiRoutes);
 
 
