50612ca1dbfb8f486ab9fc3a23ae2fec5c80e8e4
Alice <alicenara165@gmail.com>
2015-05-10 22:32:34 +0200

    events no, esdeveniments

--- server/get_events/saveData.js
+++ server/get_events/saveData.js
@@ -20,8 +20,8 @@ module.exports = function(eventsArray, callback) {
   for(var events in eventsArray){
     var e = new Event();
     e._id = eventsArray[events].id;
-    e.data_inici = transformTime(eventsArray[events].dataIni, true);
-    e.data_final = transformTime(eventsArray[events].dataFi, false);
+    e.data_inici = transformTime(eventsArray[events].dataIni, 1, true);
+    e.data_final = transformTime(eventsArray[events].dataFi, eventsArray[events].dataIni, false);
     e.nom = eventsArray[events].nom;
     e.nomLloc = eventsArray[events].nomLloc;
     e.carrer = eventsArray[events].carrer;
@@ -52,19 +52,29 @@ module.exports = function(eventsArray, callback) {
   callback("done");
 }
 
-function transformTime(data, isDate){
+function transformTime(data, dataIni, isDate){
   if(isDate){
     if(data!=""){
       var dia = data.split('/');
-    var any = dia[2].split(' ');
-    var hora = any[1].split('.');
-    var novaData = new Date(any[0],dia[1],dia[0],hora[0],hora[1]);
+      var any = dia[2].split(' ');
+      var hora = any[1].split('.');
+      var novaData = new Date(any[0],dia[1],dia[0],hora[0],hora[1]);
     }else{
       var novaData = new Date();
     }
     return novaData;
   }else{
-    var novaData = new Date();
+    if(data!=""){
+      var dia = dataIni.split('/');
+      var any = dia[2].split(' ');
+      var data = data.split('.');
+      var novaData = new Date(any[0],dia[1],dia[0],data[0],data[1]);
+    }else{
+      var dia = dataIni.split('/');
+      var any = dia[2].split(' ');
+      var hora = any[1].split('.');
+      var novaData = new Date(any[0],dia[1],dia[0],hora[0]+1,hora[1]);
+    }
     return novaData;
   }
 }
